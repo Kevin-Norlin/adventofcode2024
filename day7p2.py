3fd31012244e54d52cd980e68138578a910f31a9
Kevin Norlin <kevin.norlin@live.com>
2024-12-13 10:56:44 +0100

    Day 7

---
+++ day7p2.py
@@ -0,0 +1,73 @@
+
+
+import itertools
+import sys
+
+OPERATORS = ['*','+', '||']
+
+def get_input_data():
+    with open("./inputs/day7_input.txt") as file:
+        lines = [line.split() for line in file]
+        result = [int(line[0][:-1]) for line in lines]
+        values = [[int(element) for element in line[1:]] for line in lines]
+        return result, values
+    
+def is_possible(result, values):
+    operators = generate_operators_recursively(len(values)-1)
+    for op_sequence in operators:
+        if evaluate(values, op_sequence) == result:
+            return True
+        
+    return False
+
+def evaluate(values: list[int], op_sequence: list[str]):
+    values = list(values)
+    op_sequence = list(op_sequence)
+    res = values.pop(0) # Get first element
+
+    while True:
+        v1 = res
+        v2 = values.pop(0)
+        operator = op_sequence.pop(0)
+        res = apply_op(operator, v1, v2)
+        if len(values) == 0:
+            break
+    return res
+
+def apply_op(op: str, v1 : int, v2: int) -> int:
+    if op == '*':
+        return v1 * v2
+    if op == '+':
+        return v1 + v2
+    # Case || --> Combine 15 and 6 to 156
+    return int(str(v1) + str(v2))
+    
+def generate_operators_recursively(n : int):
+    return gen_op_rec(n, list([] for _ in range(pow(len(OPERATORS),n))))
+
+def gen_op_rec(n: int, l: list[list[str]]):
+    length = len(l)
+    if n == 0: 
+        return l
+    op = 0
+
+    for i in range(length):
+        l[i].append(OPERATORS[op % len(OPERATORS)])
+        if i % (pow(len(OPERATORS),n) // len(OPERATORS)) == 0:
+            op += 1
+            
+    return gen_op_rec(n-1, l)
+
+def main():
+    result, values = get_input_data()
+    s = 0
+
+    for result, value in zip(result, values):
+        if is_possible(result,value):
+            s += result
+
+    print(f"Part 2: {s}")
+    # Ans: 96779702119491
+
+if __name__ == "__main__":
+    main()
